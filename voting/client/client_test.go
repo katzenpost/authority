094fecf4e27c6107a62f4f7c5acd1fd7f92c0a21
David Stainton <dstainton415@gmail.com>
2018-02-19 18:53:39 +0000

    voting: client: moar unit tests

--- voting/client/client_test.go
+++ voting/client/client_test.go
@@ -23,24 +23,163 @@ import (
 	"testing"
 	"time"
 
+	"github.com/katzenpost/authority/voting/internal/s11n"
 	"github.com/katzenpost/authority/voting/server/config"
+	"github.com/katzenpost/core/crypto/ecdh"
 	"github.com/katzenpost/core/crypto/eddsa"
 	"github.com/katzenpost/core/crypto/rand"
 	"github.com/katzenpost/core/epochtime"
 	"github.com/katzenpost/core/log"
+	"github.com/katzenpost/core/pki"
+	"github.com/katzenpost/core/sphinx/constants"
+	"github.com/katzenpost/core/wire"
+	"github.com/katzenpost/core/wire/commands"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/op/go-logging.v1"
 )
+
+type descriptor struct {
+	desc *pki.MixDescriptor
+	raw  []byte
+}
+
+func generateTopology(nodeList []*descriptor, doc *pki.Document, layers int) [][][]byte {
+	nodeMap := make(map[[constants.NodeIDLength]byte]*descriptor)
+	for _, v := range nodeList {
+		id := v.desc.IdentityKey.ByteArray()
+		nodeMap[id] = v
+	}
+
+	// Since there is an existing network topology, use that as the basis for
+	// generating the mix topology such that the number of nodes per layer is
+	// approximately equal, and as many nodes as possible retain their existing
+	// layer assignment to minimise network churn.
+
+	rng := rand.NewMath()
+	targetNodesPerLayer := len(nodeList) / layers
+	topology := make([][][]byte, layers)
+
+	// Assign nodes that still exist up to the target size.
+	for layer, nodes := range doc.Topology {
+		//nodeIndexes := rng.Perm(len(nodes))
+		nodeIndexes := rng.Perm(len(nodes))
+
+		for _, idx := range nodeIndexes {
+			if len(topology[layer]) >= targetNodesPerLayer {
+				break
+			}
+
+			id := nodes[idx].IdentityKey.ByteArray()
+			if n, ok := nodeMap[id]; ok {
+				// There is a new descriptor with the same identity key,
+				// as an existing descriptor in the previous document,
+				// so preserve the layering.
+				topology[layer] = append(topology[layer], n.raw)
+				delete(nodeMap, id)
+			}
+		}
+	}
+
+	// Flatten the map containing the nodes pending assignment.
+	toAssign := make([]*descriptor, 0, len(nodeMap))
+	for _, n := range nodeMap {
+		toAssign = append(toAssign, n)
+	}
+	assignIndexes := rng.Perm(len(toAssign))
+
+	// Fill out any layers that are under the target size, by
+	// randomly assigning from the pending list.
+	idx := 0
+	for layer := range doc.Topology {
+		for len(topology[layer]) < targetNodesPerLayer {
+			n := toAssign[assignIndexes[idx]]
+			topology[layer] = append(topology[layer], n.raw)
+			idx++
+		}
+	}
+
+	// Assign the remaining nodes.
+	for layer := 0; idx < len(assignIndexes); idx++ {
+		n := toAssign[assignIndexes[idx]]
+		topology[layer] = append(topology[layer], n.raw)
+		layer++
+		layer = layer % len(topology)
+	}
+
+	return topology
+}
+
+func generateDoc(epoch uint64) ([]byte, error) {
+	mixIdentityPrivateKey, err := eddsa.NewKeypair(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	targetMix := &pki.MixDescriptor{
+		Name:        "NSA_Spy_Sattelite_Mix001",
+		IdentityKey: mixIdentityPrivateKey.PublicKey(),
+		LinkKey:     nil,
+		MixKeys:     nil,
+		Addresses:   nil,
+		Kaetzchen:   nil,
+		Layer:       1,
+		LoadWeight:  0,
+	}
+	pdoc := &pki.Document{
+		Epoch:           epoch,
+		MixLambda:       3.141,
+		MixMaxDelay:     3,
+		SendLambda:      2.6,
+		SendShift:       2,
+		SendMaxInterval: 42,
+		Topology: [][]*pki.MixDescriptor{
+			[]*pki.MixDescriptor{
+				targetMix,
+			},
+		},
+		Providers: []*pki.MixDescriptor{
+			targetMix,
+		},
+	}
+	signed, err := s11n.SignDescriptor(mixIdentityPrivateKey, targetMix)
+	if err != nil {
+		return nil, err
+	}
+	nodeList := []*descriptor{
+		&descriptor{
+			raw:  []byte(signed),
+			desc: targetMix,
+		},
+	}
+	topology := generateTopology(nodeList, pdoc, 3)
+	doc := &s11n.Document{
+		Epoch:           epoch,
+		MixLambda:       3.141,
+		MixMaxDelay:     3,
+		SendLambda:      2.6,
+		SendShift:       2,
+		SendMaxInterval: 42,
+		Topology:        topology,
+		Providers:       [][]byte{[]byte{}},
+	}
+	signed, err = s11n.MultiSignDocument(mixIdentityPrivateKey, nil, doc)
+	if err != nil {
+		return nil, err
+	}
+	return []byte(signed), nil
+}
 
 type mockDialer struct {
 	serverConn net.Conn
 	clientConn net.Conn
 	dialCh     chan interface{}
+	log        *logging.Logger
 }
 
-func newMockDialer() *mockDialer {
+func newMockDialer(logBackend *log.Backend) *mockDialer {
 	d := new(mockDialer)
 	d.serverConn, d.clientConn = net.Pipe()
 	d.dialCh = make(chan interface{}, 0)
+	d.log = logBackend.GetLogger("mockDialer: ")
 	return d
 }
 
@@ -52,17 +191,69 @@ func (d *mockDialer) waitUntilDialed() {
 	<-d.dialCh
 }
 
+func (d *mockDialer) mockServer(authPrivateKey *ecdh.PrivateKey) {
+	d.waitUntilDialed()
+	cfg := &wire.SessionConfig{
+		Authenticator:     d,
+		AdditionalData:    nil,
+		AuthenticationKey: authPrivateKey,
+		RandomReader:      rand.Reader,
+	}
+	session, err := wire.NewSession(cfg, false)
+	if err != nil {
+		d.log.Errorf("mockServer NewSession failure: %s", err)
+		return
+	}
+	defer session.Close()
+	err = session.Initialize(d.serverConn)
+	if err != nil {
+		d.log.Errorf("mockServer session Initialize failure: %s", err)
+		return
+	}
+	cmd, err := session.RecvCommand()
+	if err != nil {
+		d.log.Errorf("mockServer session RecvCommand failure: %s", err)
+		return
+	}
+	switch c := cmd.(type) {
+	case *commands.GetConsensus:
+		rawDoc, err := generateDoc(c.Epoch)
+		if err != nil {
+			d.log.Errorf("mockServer session generateDoc failure: %s", err)
+			return
+		}
+
+		reply := &commands.Consensus{
+			ErrorCode: commands.ConsensusOk,
+			Payload:   rawDoc,
+		}
+		err = session.SendCommand(reply)
+		if err != nil {
+			d.log.Errorf("SendCommand failure: %s", err)
+			return
+		}
+	default:
+		return
+	}
+}
+
+func (d *mockDialer) IsPeerValid(creds *wire.PeerCredentials) bool {
+	// XXX
+	return true
+}
+
 func TestClient(t *testing.T) {
 	assert := assert.New(t)
 
-	dialer := newMockDialer()
-
 	logBackend, err := log.New("", "DEBUG", false)
 	assert.NoError(err, "wtf")
+	dialer := newMockDialer(logBackend)
 
 	peer1IdentityPrivateKey, err := eddsa.NewKeypair(rand.Reader)
 	assert.NoError(err, "wtf")
 	peer1LinkPrivateKey := peer1IdentityPrivateKey.ToECDH()
+
+	go dialer.mockServer(peer1LinkPrivateKey)
 
 	cfg := &Config{
 		LogBackend: logBackend,
